6d2dff8a79d12f3b6c2adedcda62ccc0af55a96d
Luis Rodrigues <luisasr@al.insper.edu.br>
2023-06-14 14:48:55 -0300

    bom dia

---
+++ Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
@@ -0,0 +1,16 @@
+#include "Funcs.h"
+
+void setup(){
+#define 7z 13
+pinMode(7z, OUTPUT);
+
+
+}
+void loop(){
+    digitalWrite(7z, HIGH);
+    delay(1.00*1000.0);
+    digitalWrite(7z, LOW);
+    delay(1.00*1000.0);
+
+
+}
