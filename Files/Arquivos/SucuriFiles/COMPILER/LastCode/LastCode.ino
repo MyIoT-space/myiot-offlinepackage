1048abb7831a59be9f1af7aca603c6c94276930e
HelioZF <daniel.minsonp@gmail.com>
2023-08-21 15:29:48 -0300

    CONSOLE EM INGLES

--- Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
+++ Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
@@ -1,58 +1,16 @@
 #include "Funcs.h"
 
-void mover_motor1(int pino1, int pino2, int dir)
-{
-  if(dir==0){
-    digitalWrite(pino1, LOW);
-    digitalWrite(pino2, LOW);
-  }
-  else if(dir==1){
-    digitalWrite(pino1, LOW);
-    digitalWrite(pino2, HIGH);
-  }
-  else if(dir==2){
-    digitalWrite(pino1, HIGH);
-    digitalWrite(pino2, LOW);
-  }
-}
-
-void mover_motor2(int pino1, int pino2, int dir)
-{
-  if(dir==0){
-    digitalWrite(pino1, LOW);
-    digitalWrite(pino2, LOW);
-  }
-  else if(dir==1){
-    digitalWrite(pino1, LOW);
-    digitalWrite(pino2, HIGH);
-  }
-  else if(dir==2){
-    digitalWrite(pino1, HIGH);
-    digitalWrite(pino2, LOW);
-  }
-}
-
 void setup(){
-
-
-
-
-
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-
-pinMode(7, OUTPUT);
-pinMode(6, OUTPUT);
+#define D13 13
+pinMode(D13, OUTPUT);
 
 
 }
 void loop(){
-mover_motor1(5, 4, 2);
-mover_motor2(6, 7, 2);
-    delay(2.00*1000.0);
-mover_motor1(5, 4, 2);
-mover_motor2(6, 7, 1);
-    delay(0.50*1000.0);
+    digitalWrite(D13, HIGH);
+    delay(1.00*1000.0);
+    digitalWrite(D13, LOW);
+    delay(1.00*1000.0);
 
 
 }
