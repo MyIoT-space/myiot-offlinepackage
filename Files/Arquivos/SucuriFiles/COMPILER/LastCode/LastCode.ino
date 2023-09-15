2e9fe00daf4597f643ca343d801c8d88c77c515e
HelioZF <daniel.minsonp@gmail.com>
2023-09-15 14:20:29 -0300

    broker atualizado

--- Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
+++ Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
@@ -4,15 +4,21 @@
 void setup(){
 
 
+#define D13 13
+pinMode(D13, OUTPUT);
 
 Serial.begin(9600);
 
 
 }
 void loop(){
-    Serial.println("#whatsapp de:meu deus para: 5511973020144 mensagem: amado, fique bem");
+    if(ler_serial() == "a"){
+        digitalWrite(D13, HIGH);
+        delay(1.00*1000.0);
+    }
 
-    delay(10.00*1000.0);
+    digitalWrite(D13, LOW);
+    delay(2.00*1000.0);
 
 
 }
