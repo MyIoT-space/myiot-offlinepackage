c20120dc15f6e80ee7be1723f021170270bf3390
HelioZF <daniel.minsonp@gmail.com>
2023-09-22 15:02:39 -0300

    Agora da erro de serialrx quando precisa dela

--- Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
+++ Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
@@ -1,16 +1,16 @@
 #include "Funcs.h"
 
+
 void setup(){
-#define D13 13
-pinMode(D13, OUTPUT);
-int A0 = A0;
+
+
+
+Serial.begin(9600);
 
 
 }
 void loop(){
-    if(analogRead(A0) > 1023){
-        digitalWrite(D13, HIGH);
-    }
+    Serial.println("#mensagem dadsasdasd+_&@%$$_++_&@%$$_+");
 
 
 
