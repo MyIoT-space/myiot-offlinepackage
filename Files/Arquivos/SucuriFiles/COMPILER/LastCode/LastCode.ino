f14174ace715c97a44b06220c7fe6dd9bb9e10f4
HelioZF <daniel.minsonp@gmail.com>
2023-10-25 16:05:52 -0300

    pequenos bugs da placa rj kids

--- Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
+++ Files/Arquivos/SucuriFiles/COMPILER/LastCode/LastCode.ino
@@ -1,13 +1,21 @@
 #include "Funcs.h"
 
 void setup(){
-#define D11 11
-pinMode(D11, OUTPUT);
+#define D2(BIP) 2
+pinMode(D2(BIP), OUTPUT);
+#define D9 9
+pinMode(D9, INPUT);
 
 
 }
 void loop(){
-    digitalWrite(D11, HIGH);
+    if(digitalRead(D9) == HIGH){
+        digitalWrite(D2(BIP), HIGH);
+        delay(1.00*1000.0);
+    }
+
+    digitalWrite(D2(BIP), LOW);
+    delay(1.00*1000.0);
 
 
 }
